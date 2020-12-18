ff55c32fa98660bc1a82737804c18e8a87b00a8f
oscar-davids <oscar_davids@outlook.com>
2020-12-18 22:21:26 +0800

    added DBSCAN test code with multi vector

--- dbscanmp_test.go
+++ dbscanmp_test.go
@@ -8,23 +8,42 @@ import (
 )
 
 type MultiPoint struct {
+	index    int
+	pos      []float64
 	position float64
 }
 
 func (s MultiPoint) DistanceTo(c Point) float64 {
-	distance := math.Abs(c.(MultiPoint).position - s.position)
-	return distance
+	distance := 0.0
+	dotproduct := 0.0
+	s1 := 0.0
+	s2 := 0.0
+	if len(c.(MultiPoint).pos) != len(s.pos) {
+		distance = 2.0
+		return distance
+	}
+
+	for i := 0; i < len(c.(MultiPoint).pos); i++ {
+		dotproduct += c.(MultiPoint).pos[i] * s.pos[i]
+		s1 += s.pos[i] * s.pos[i]
+		s2 += c.(MultiPoint).pos[i] * c.(MultiPoint).pos[i]
+	}
+
+	distance = dotproduct / (math.Sqrt(s1) * math.Sqrt(s2))
+
+	//log.Println("distance:", 1.0-distance)
+	return 1.0 - distance
 }
 
 func (s MultiPoint) Name() string {
-	return fmt.Sprint(s.position)
+	return fmt.Sprint(s.index)
 }
 
 func TestMpPutAll(t *testing.T) {
 	testMap := make(map[string]Point)
 	clusterList := []Point{
-		MultiPoint{10},
-		MultiPoint{12},
+		MultiPoint{index: 1, pos: []float64{1.0, 2.0, 3.0}},
+		MultiPoint{index: 2, pos: []float64{1.0, 2.0, 3.0}},
 	}
 	merge(testMap, clusterList...)
 	mapSize := len(testMap)
@@ -37,33 +56,36 @@ func TestMpPutAll(t *testing.T) {
 func TestMpFindNeighbours(t *testing.T) {
 	log.Println("Executing TestMpFindNeighbours")
 	clusterList := []Point{
-		MultiPoint{0},
-		MultiPoint{1},
-		MultiPoint{-1},
-		MultiPoint{1.5},
-		MultiPoint{-0.5},
+		MultiPoint{index: 1, pos: []float64{29328, 97760, 160552, 197400, 207740, 321292, 541252, 579792, 625476, 681312}},
+		MultiPoint{index: 2, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
+		MultiPoint{index: 3, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
+		MultiPoint{index: 4, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
+		MultiPoint{index: 5, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
 	}
 
-	eps := 1.01
+	eps := 0.0015
 	neighbours := findNeighbours(clusterList[0], clusterList, eps)
-	if 3 != len(neighbours) {
+
+	log.Println("neighbours counts:", len(neighbours))
+
+	if 4 != len(neighbours) {
 		t.Error("Mismatched neighbours counts")
 	}
 }
 
 func TestMpExpandCluster(t *testing.T) {
 	log.Println("Executing TestMpExpandCluster")
-	expected := 4
+	expected := 5
 	clusterList := []Point{
-		MultiPoint{0},
-		MultiPoint{1},
-		MultiPoint{2},
-		MultiPoint{2.1},
-		MultiPoint{5},
+		MultiPoint{index: 1, pos: []float64{29328, 97760, 160552, 197400, 207740, 321292, 541252, 579792, 625476, 681312}},
+		MultiPoint{index: 2, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
+		MultiPoint{index: 3, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
+		MultiPoint{index: 4, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
+		MultiPoint{index: 5, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
 	}
 
-	eps := 1.0
-	minPts := 1
+	eps := 0.0015
+	minPts := 2
 	visitMap := make(map[string]bool)
 	cluster := make([]Point, 0)
 	cluster = expandCluster(cluster, clusterList, visitMap, minPts, eps)
@@ -73,17 +95,23 @@ func TestMpExpandCluster(t *testing.T) {
 }
 
 func TestMpCluster(t *testing.T) {
-	clusters := Cluster(2, 1.0,
-		MultiPoint{1},
-		MultiPoint{0.5},
-		MultiPoint{0},
-		MultiPoint{5},
-		MultiPoint{4.5},
-		MultiPoint{4})
+	clusters := Cluster(2, 0.0015,
+		MultiPoint{index: 1, pos: []float64{29328, 97760, 160552, 197400, 207740, 321292, 541252, 579792, 625476, 681312}},
+		MultiPoint{index: 2, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
+		MultiPoint{index: 3, pos: []float64{29328, 97948, 160552, 197400, 207740, 321668, 541252, 579604, 623596, 680372}},
+		MultiPoint{index: 4, pos: []float64{883976, 1025728, 1210720, 1263924, 1554196, 1962344, 2316724, 3441528, 0, 0}},
+		MultiPoint{index: 5, pos: []float64{896948, 1030616, 1216360, 1256780, 1595744, 2021564, 2394744, 3639304, 0, 0}},
+		MultiPoint{index: 6, pos: []float64{896948, 1030616, 1216360, 1256780, 1595744, 2021564, 2394744, 3639304, 0, 0}},
+	)
+
+	log.Println("cluster counts:", len(clusters))
 
 	if 2 == len(clusters) {
 		if 3 != len(clusters[0]) || 3 != len(clusters[1]) {
 			t.Error("Mismatched cluster member counts")
+		} else {
+			log.Println("cluster names:", clusters[0][0].Name(), clusters[0][1].Name(), clusters[0][2].Name())
+			log.Println("cluster names:", clusters[1][0].Name(), clusters[1][1].Name(), clusters[1][2].Name())
 		}
 	} else {
 		t.Error("Mismatched cluster counts")
