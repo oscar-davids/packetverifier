d324935330ccefea01a0833a227c2eec925ce842
oscar-davids <oscar_davids@outlook.com>
2020-12-30 23:35:15 +0800

    negative dataset

--- calcdifffeatures.py
+++ calcdifffeatures.py
@@ -3,6 +3,7 @@ import argparse
 import datetime
 import csv
 import numpy as np
+import random
 from verifier import Verifier
 
 if __name__ == "__main__":
@@ -10,11 +11,13 @@ if __name__ == "__main__":
     parser.add_argument("--srcdir", default="", help="source video file directory")
     parser.add_argument("--renddir", default="", help="rendition video file directory")
     parser.add_argument("--infile", default="in.csv", help="csv file to calculate diff features.")
+    parser.add_argument("--negative", default=0, type=int, help="csv file to calculate diff features.")
     args = parser.parse_args()
 
     infile = args.infile
     srcdir = args.srcdir
     renddir = args.renddir
+    negative = args.negative
 
     outcsv = "difffeature" + datetime.datetime.now().strftime("%m%d%H") + ".csv"
     fileout = open(outcsv, 'w', newline='')
@@ -33,27 +36,44 @@ if __name__ == "__main__":
             print( str(logcount) + "-----" + "start")
             logcount = logcount + 1
             wrow = []
+            difffeatures = ""
             if brheader == False:
                 brheader = True
             else:
                 srcfile = srcdir + "/" + row[0]
                 rendfile = renddir + "/" + row[9].replace('"','')
                 indicies = row[8]
-
-                file_stats = os.stat(srcfile)
-
-                result = verifier.getfeature(srcfile, [{'uri': rendfile}], indicies)
-
-                difffeatures = np.array2string(result[0]['difffeature'], precision=10, separator=',',
-                                suppress_small=False)
-                difffeatures = difffeatures.replace('[','')
-                difffeatures = difffeatures.replace(']', '')
-                difffeatures = difffeatures.replace('\n', '')
-                difffeatures = '"' + difffeatures + '"'
-
                 wrow = row
-                wrow.append(difffeatures)
-                wrow.append(result[0]['tamper'])
+
+                if negative != 0:
+                    if logcount%2 == 0:
+                        #create randomize features
+                        list_rx = [random.uniform(0, 1000000) for i in range(6)]
+                        difffeatures = '"'
+                        for i in range(5):
+                            difffeatures += (str(list_rx[i]) + ",")
+                        difffeatures += str(list_rx[5])
+                        difffeatures += '"'
+                    else:
+                        # create zero features
+                        difffeatures = '"' + '0.0,0.0,0.0,0.0,0.0,0.0' + '"'
+
+                    wrow.append(difffeatures)
+                    wrow.append(1)
+                else:
+                    file_stats = os.stat(srcfile)
+                    result = verifier.getfeature(srcfile, [{'uri': rendfile}], indicies)
+
+                    difffeatures = np.array2string(result[0]['difffeature'], precision=10, separator=',',
+                                    suppress_small=False)
+                    difffeatures = difffeatures.replace('[','')
+                    difffeatures = difffeatures.replace(']', '')
+                    difffeatures = difffeatures.replace('\n', '')
+                    difffeatures = '"' + difffeatures + '"'
+
+
+                    wrow.append(difffeatures)
+                    wrow.append(result[0]['tamper'])
 
                 wr.writerow(wrow)
 
