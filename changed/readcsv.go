48f324332e40942fdd74fef3b095cabca0ed0eff
Oscar <54097108+oscar-davids@users.noreply.github.com>
2021-02-03 07:15:53 +0800

    Create readcsv.go

---
+++ changed/readcsv.go
@@ -0,0 +1,24 @@
+func readcsvmap(reader io.Reader) []map[string]string {
+	r := csv.NewReader(reader)
+	rows := []map[string]string{}
+	var header []string
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if header == nil {
+			header = record
+		} else {
+			dict := map[string]string{}
+			for i := range header {
+				dict[header[i]] = record[i]
+			}
+			rows = append(rows, dict)
+		}
+	}
+	return rows
+}
