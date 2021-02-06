511e2091dbfe8c8c20af76097b62bbf6f6272863
unknown <oscar_davids@outlook.com>
2021-02-06 18:33:56 +0800

    accuracy check

--- calcdifffeatures.py
+++ calcdifffeatures.py
@@ -7,17 +7,20 @@ import random
 from verifier import Verifier
 
 if __name__ == "__main__":
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("--srcdir", default="", help="source video file directory")
     parser.add_argument("--renddir", default="", help="rendition video file directory")
     parser.add_argument("--infile", default="in.csv", help="csv file to calculate diff features.")
-    parser.add_argument("--negative", default=0, type=int, help="csv file to calculate diff features.")
+    parser.add_argument("--calctype", default=0, type=int, help="csv file to calculate diff features.")
     args = parser.parse_args()
 
     infile = args.infile
     srcdir = args.srcdir
     renddir = args.renddir
-    negative = args.negative
+    calctype = args.calctype
+    #calctype: 0-difffeture 1-accuracy 2-negative(skip or zero)
+    
 
     outcsv = "difffeature" + datetime.datetime.now().strftime("%m%d%H%M%S") + ".csv"
     fileout = open(outcsv, 'w', newline='')
@@ -27,7 +30,8 @@ if __name__ == "__main__":
     max_samples = 10
     debug = False
     logcount = 0
-    verifier = Verifier(10, 'verification-metamodel-2020-07-06.tar.xz', False, False, debug)
+    #'http://storage.googleapis.com/verification-models/verification-metamodel-fps2.tar.xz'    
+    verifier = Verifier(10, 'http://storage.googleapis.com/verification-models/verification-metamodel-2020-07-06.tar.xz', False, False, debug)   
 
     brheader = False
     with open(infile) as csvfile:
@@ -45,7 +49,29 @@ if __name__ == "__main__":
                 indicies = row[8]
                 wrow = row
 
-                if negative != 0:
+                if calctype == 0: #diff calc
+                    file_stats = os.stat(srcfile)
+                    result = verifier.getfeature(srcfile, [{'uri': rendfile}], indicies)
+
+                    difffeatures = np.array2string(result[0]['difffeature'], precision=10, separator=',',
+                                    suppress_small=False)
+                    difffeatures = difffeatures.replace('[','')
+                    difffeatures = difffeatures.replace(']', '')
+                    difffeatures = difffeatures.replace('\n', '')
+                    difffeatures = '"' + difffeatures + '"'
+
+                    wrow.append(difffeatures)
+                    wrow.append(result[0]['tamper'])
+
+                elif calctype == 1: #calc accuracy
+                   
+                    result = verifier.verify(srcfile, [{'uri': rendfile}])
+                    difffeatures = '"' + '1.0,1.0,1.0,1.0,1.0,1.0' + '"'
+                    wrow.append(difffeatures)
+                    wrow.append(result[0]['tamper'])
+                    print( "result :" + str(result[0]['tamper']))
+                    
+                elif calctype == 2:
                     if logcount%2 == 0:
                         #create randomize features
                         list_rx = [random.uniform(0, 1000000) for i in range(6)]
@@ -60,20 +86,6 @@ if __name__ == "__main__":
 
                     wrow.append(difffeatures)
                     wrow.append(1)
-                else:
-                    file_stats = os.stat(srcfile)
-                    result = verifier.getfeature(srcfile, [{'uri': rendfile}], indicies)
-
-                    difffeatures = np.array2string(result[0]['difffeature'], precision=10, separator=',',
-                                    suppress_small=False)
-                    difffeatures = difffeatures.replace('[','')
-                    difffeatures = difffeatures.replace(']', '')
-                    difffeatures = difffeatures.replace('\n', '')
-                    difffeatures = '"' + difffeatures + '"'
-
-
-                    wrow.append(difffeatures)
-                    wrow.append(result[0]['tamper'])
 
                 wr.writerow(wrow)
 
